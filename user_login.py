9fe8534f2722bc3b85dfc7eff2c5997747274041
Sri Harsha <codewithharsha@gmail.com>
2025-11-06 18:37:23 +0530

    add block_user

--- user_login.py
+++ user_login.py
@@ -6,28 +6,44 @@ import getpass
 
 class LoginPage:
 
+    ATTEMPTS=3
+
     def __init__(self):
-        self.bank_db = BankDataBase()
+        self.bankdatabase = BankDataBase()
+        self.attempt_count=0
         
     def auth_creds(self):
-        self.auth_user=self.bank_db.auth_user(self.user_id,self.user_pswd)
-       
+        self.auth_user=self.bankdatabase.auth_user(self.user_name,self.user_password)
+        
         if self.auth_user:
-            self.show_homepage=UserSession(self.user_id,self.bank_db)
+            self.show_homepage=UserSession(self.user_name,self.bankdatabase)
             self.show_homepage.show_dashboard()
         else:
+            self.attempt_count+=1
+            self.block_user(self.attempt_count)
+
             print("Please enter correct creds")
+
+    def block_user(self):
+        attempts=3
+        
+        if self.attemt_count > attempts:
+            print("You Have Blocked for 24 hours") #block user for 24 hours
+            #create a new data table for login and log out timings and checking user account status
 
 
     def Show_login_page(self): # Validation of len and multiple attempts are pending
         print(f"Welcome To SREE BANK OF INDIA\n")
-        self.user_id=input("Please Enter Your User Name: ").strip().lower()
-        self.user_pswd=getpass.getpass("Please Enter Your Password: ").strip()
-        
-        self.auth_creds()
+        self.user_name=input("Please Enter Your User Name: ").strip().lower()
+        self.user_password=getpass.getpass("Please Enter Your Password: ").strip()
+        input("Press Enter To Continue")
+        if(len(self.user_name)>5 and len(self.user_password)>7):
+            self.auth_creds()
+        else:
+            print("Please Enter Vaild Username and Password")
 
 
-#AttributeError: 'OnlineBankApp' object has no attribute 'db'
+
 
 if __name__=="__main__" :
     LoginPage().Show_login_page()
